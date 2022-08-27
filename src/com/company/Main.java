a60dd5c8b82488b01d6c762895362c100e90cea4
stave031 <stevealba7382@gmail.com>
2022-08-27 13:08:43 -0400

    Feature SUN added but with Errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,14 @@ public class Main {
         inception.moon=4;
         inception.planets=2;
         System.out.println("the basic structure is ready");
+
+        //developing the feature SUN
+        SUN omaro=new SUN();
+        omaro.temperature="12334433kj";
+        omaro.name="ferro";
+        omaro.radius=12344555454545455454;
+        omaro.color="pink";
     }
+
+
 }
