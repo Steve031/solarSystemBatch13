42306b3faab442887eff40d4b5bcab4b13070976
stave031 <stevealba7382@gmail.com>
2022-08-28 10:54:47 -0400

    Planet1 added and is error free

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,6 +15,13 @@ public class Main {
         omaro.name="ferro";
         omaro.radius=1234455;
         omaro.color="pink";
+
+        //developing Planet1
+        Planet1 superplanet=new Planet1();
+        superplanet.color="magenta";
+        superplanet.name="hash";
+        superplanet.size="545488km";
+        
     }
 
 
